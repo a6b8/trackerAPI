833fca9b72e3a20eecb17866346ab1df662ad65c
αηdr3α5 ɮαηɦօlʐ3ʀ <AndreasBanholzer@Me.com>
2024-11-23 02:14:51 +0100

    Include test (#2)

---
+++ tests/circle-ci.mjs
@@ -0,0 +1,13 @@
+import { SolanaTracker } from './src/index.mjs'
+
+const apiKey = '<<API_KEY>>'
+const st = new SolanaTracker( { apiKey } )
+const status = st.health()
+
+if( status ) {
+    console.log( 'Success' )
+    process.exit( 0 )
+} else {
+    console.log( 'Failure' )
+    process.exit( 1 )
+}
